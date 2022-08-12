9722870c9f1db81249565feeae61b36d12d9750a
LNThanhNhan <20520667@gm.uit.edu.vn>
2022-08-12 18:36:52 +0700

    Updates

---
+++ precache-manifest.0c975aff689bcc4234d4c42d2490bd0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "eebf50601b11d2c6b2d6",
+    "url": "/Info_site_with_React/static/css/main.c6716783.chunk.css"
+  },
+  {
+    "revision": "eebf50601b11d2c6b2d6",
+    "url": "/Info_site_with_React/static/js/main.fd330e24.chunk.js"
+  },
+  {
+    "revision": "8262ec2da5058353dc8a",
+    "url": "/Info_site_with_React/static/js/runtime~main.776d90e3.js"
+  },
+  {
+    "revision": "63095527fa19b53dfeb4",
+    "url": "/Info_site_with_React/static/js/2.a4960ed6.chunk.js"
+  },
+  {
+    "revision": "6aab517d6c3d856a0e4d410f7df6b254",
+    "url": "/Info_site_with_React/static/media/IMG_20220215_120724.6aab517d.png"
+  },
+  {
+    "revision": "d8ff4127a275dfbdf3b455c5fb8b42af",
+    "url": "/Info_site_with_React/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Info_site_with_React/precache-manifest.0c975aff689bcc4234d4c42d2490bd0e.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Info_site_with_React/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
